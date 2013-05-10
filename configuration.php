23f004484a2148e05e6ecd86d94b641564def6b5
simonp <simon.plasger@web.de>
2013-05-11 01:00:55 +0200

    Change example config

--- configuration.php
+++ configuration.php
@@ -4,5 +4,5 @@ defube("IRC_PASS", "password"); // Password the user uses to authenticate
 define("IRC_IRCPASS", "password"); // Password for the connection
 define("IRC_SERVER", "irc.server.net");
 define("IRC_PORT", 6667);
-define("IRC_CONFIGFILE", "whitelist.conf");
+define("IRC_CONFIGFILE", "/path/to/directory/whitelist.conf");
 ?>
