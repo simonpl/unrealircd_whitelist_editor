4fab8a2374f5e759fd6f44581cb8c5c53888945f
simonp <simon.plasger@web.de>
2013-05-10 22:18:21 +0200

    Added Comment

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
+/*
+    Unreal IRCd Whitelist Editor let's you edit the channel whitelist of an Unreal IRCd
+    Copyright (C) 2013 Simon Plasger
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    See COPYING for details.
+    
+    See README for informations about Setup and Usage.
+*/
 require "configuration.php";
 if(!isset($_GET['post']))
 {
