b9c8a374cd1628c954710aa9731ec13f7e5f2d16
simonp <simon.plasger@web.de>
2013-05-11 11:07:34 +0200

    Bugfix

--- configuration.php
+++ configuration.php
@@ -1,6 +1,6 @@
 <?php
 define("IRC_USER", "rehashbot");
-defube("IRC_PASS", "password"); // Password the user uses to authenticate
+define("IRC_PASS", "password"); // Password the user uses to authenticate
 define("IRC_IRCPASS", "password"); // Password for the connection
 define("IRC_SERVER", "irc.server.net");
 define("IRC_PORT", 6667);
